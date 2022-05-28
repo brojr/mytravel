f523fbc95678eb2eda34b381f9c483ae5a03a5a4
sm970309 <sm970309@gmail.com>
2022-05-28 23:48:33 +0900

    Near 수정[카카오맵 불러오기]

--- src/routers/Near.js
+++ src/routers/Near.js
@@ -1,14 +1,27 @@
+/* global kakao */
+import { useEffect } from "react";
 import { useLocation } from "react-router-dom"
 
 const Near = () =>{
     const {state} = useLocation();
+    useEffect(()=>{
+        let container = document.getElementById('map');
+        let options = {
+            center: new window.kakao.maps.LatLng(37.52645515114125, 126.85272799966204),
+            level: 6,
+        }
+        let map = new window.kakao.maps.Map(container,options)
+    },[])
     return(
     <div>
         {state?
         <h2>Searching {state}...</h2>    :
         <h2>Searching Nerby...</h2>
     }
-    {/* 네이버 맵 연동 -> 주변 맛집,놀거리 찾기 */}
+    {/* 카카오 맵 연동 -> 주변 맛집,놀거리 찾기 */}
+    <div>
+        <div id='map' style={{width:'100%',height:"400px"}}></div>        
+    </div>
     </div>
     
     )
