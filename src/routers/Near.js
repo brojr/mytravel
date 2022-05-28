c24db3cd317337751096ed48786cceaf4bb9fb65
sm970309 <sm970309@gmail.com>
2022-05-29 00:13:00 +0900

    Near 수정[위치 마커]

--- src/routers/Near.js
+++ src/routers/Near.js
@@ -5,12 +5,31 @@ import { useLocation } from "react-router-dom"
 const Near = () =>{
     const {state} = useLocation();
     useEffect(()=>{
-        let container = document.getElementById('map');
-        let options = {
-            center: new window.kakao.maps.LatLng(37.52645515114125, 126.85272799966204),
+        const container = document.getElementById('map');
+        const options = {
+            center: new kakao.maps.LatLng(33.450701, 126.570667),
             level: 6,
         }
-        let map = new window.kakao.maps.Map(container,options)
+        const map = new kakao.maps.Map(container,options)
+        const geocoder = new kakao.maps.services.Geocoder();
+        geocoder.addressSearch('서울특별시 신정중앙로 8길 11',
+        (result,status) =>{
+            if (status===kakao.maps.services.Status.OK){
+                const coords = new kakao.maps.LatLng(result[0].y,result[0].x);
+
+                const marker = new kakao.maps.Marker({
+                    map:map,
+                    position:coords
+                })
+                map.setCenter(coords)
+                let infowindow = new kakao.maps.InfoWindow({
+                    content:'<div style="width:150px;text-align:center;padding:6px 0;">장소설명</div>'
+                })
+                infowindow.open(map,marker)
+            }
+        }    
+        )
+
     },[])
     return(
     <div>
