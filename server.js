5052dc584a9258cdcc0cc3a4db8849b7a4ffaf4e
sm970309 <sm970309@gmail.com>
2022-06-07 15:43:57 +0900

    node express, react 연동

--- server.js
+++ server.js
@@ -2,11 +2,16 @@ const express = require('express')
 const path = require('path')
 const app = express();
 
-app.use(express.static(path.join(__dirname,'client/public')))
+app.use(express.static(path.join(__dirname,'client/build')))
 app.get('/',(req,res)=>{
-    res.sendFile(path.join(__dirname,'client/public/index.html'))
+    res.sendFile(path.join(__dirname,'client/build/index.html'))
 });
 
 app.listen(8080,() =>{
-    console.log('hi')
+    console.log('Listening port 8080...')
+});
+
+// 라우팅은 리액트에서 실행
+app.get('*',(req,res)=>{
+    res.sendFile(path.join(__dirname,'client/build/index.html'))
 });
