4b3b3923945a02c6eac4a031fa8dfd5542d42f05
sm970309 <sm970309@gmail.com>
2022-06-08 12:24:31 +0900

    Login.js[POST로 데이터전달]

--- client/src/routers/Login.js
+++ client/src/routers/Login.js
@@ -1,9 +1,47 @@
+import axios from "axios"
+import { useState } from "react"
 const Login = ({ isLogin }) => {
+    const [id, setId] = useState('');
+    const [pw, setPw] = useState('');
+    const onChange = (event) => {
+        const { name } = event.target
+        if (name === "id") {
+            setId(event.target.value)
+        }
+        else {
+            setPw(event.target.value)
+        }
+
+    }
+    const onSubmit = async (event) => {
+        event.preventDefault();
+        // POST형식으로 데이터 서버에 전달
+        const response = await axios.post('http://localhost:8080/register', {
+            id: id,
+            pw: pw
+        })
+        console.log(response.status)
+    }
+
     return (
         <div>
             {isLogin ?
-                <div>profile</div>:
-                <div>Login page</div> 
+                <div>profile</div> :
+                <form onSubmit={onSubmit}>
+                    <input
+                        name='id'
+                        value={id}
+                        type="text"
+                        placeholder="ID"
+                        onChange={onChange} />
+                    <input
+                        name='pw'
+                        value={pw}
+                        type="password"
+                        placeholder="PW"
+                        onChange={onChange} />
+                    <input type="submit" value="로그인" />
+                </form>
             }
 
         </div>
--- server/server.js
+++ server/server.js
@@ -1,6 +1,11 @@
 const express = require('express')
 const path = require('path')
 const app = express();
+const PORT = 8080;
+const bodyParser = require('body-parser')
+const cors = require('cors')
+
+// DB부분
 const mongoose = require('mongoose');
 
 const uri = 'mongodb://localhost:27017/my_db'
@@ -12,18 +17,27 @@ const db = mongoose.connect(uri,(err)=>{
         console.log('Connected')
     }
 })
-console.log(mongoose)
+// console.log(mongoose)
 
+app.use(cors()) // 다른 포트와 통신하기 위해 필요
+app.use(bodyParser.json()) // POST형식에서 데이터를 받기위해 필요
 app.use(express.static(path.join(__dirname,'..','client/build')))
+
 app.get('/',(req,res)=>{
+    console.log(req.body)
     res.sendFile(path.join(__dirname,'..','client/build/index.html'))
 });
 
-app.listen(8080,() =>{
-    console.log('Listening port 8080...')
+app.post('/register',(req,res)=>{
+    console.log(req.body)
+    res.send('This is 8080')
+})
+
+app.listen(PORT,() =>{
+    console.log(`Listening port ${PORT}...`)
 });
 
 // 라우팅은 리액트에서 실행
 app.get('*',(req,res)=>{
-    res.sendFile(path.join(__dirname,'client/build/index.html'))
+    res.sendFile(path.join(__dirname,'..','client/build/index.html'))
 });
